f1d23e792d6f6320f0a31c68a84905f0bba946c2
SoniEx2 <endermoneymod@gmail.com>
2013-08-19 23:02:54 -0300

    More tweaks to IBlockPlayer

--- src/com/github/soniex2/nbx/api/IBlockPlayer.java
+++ src/com/github/soniex2/nbx/api/IBlockPlayer.java
@@ -7,22 +7,59 @@ public interface IBlockPlayer {
 
 	/**
 	 * Plays a NBSBlock.
-	 * @param block the NBSBlock
+	 * 
+	 * @param block
+	 *            the NBSBlock
 	 */
 	public void play(NBSBlock block);
 
 	/**
 	 * Plays a NBSBlock at a given volume.
-	 * @param block the NBSBlock
-	 * @param volume the volume
+	 * 
+	 * @param block
+	 *            the NBSBlock
+	 * @param volume
+	 *            the volume
 	 */
 	public void play(NBSBlock block, float volume);
 
 	/**
 	 * Plays all notes in a NBSTick.
-	 * @param tick the NBSTick to play the notes from
+	 * 
+	 * @param tick
+	 *            the NBSTick to play the notes from
 	 */
 	public void play(NBSTick tick);
+
+	/**
+	 * Plays all notes in a NBSTick.
+	 * 
+	 * @param tick
+	 *            the NBSTick to play the notes from
+	 * @param volumes
+	 *            a volume array
+	 */
+	public void play(NBSTick tick, byte[] volumes);
+
+	/**
+	 * Plays all notes in a NBSTick.
+	 * 
+	 * @param tick
+	 *            the NBSTick to play the notes from
+	 * @param volumes
+	 *            a volume array
+	 */
+	public void play(NBSTick tick, int[] volumes);
+
+	/**
+	 * Plays all notes in a NBSTick.
+	 * 
+	 * @param tick
+	 *            the NBSTick to play the notes from
+	 * @param volumes
+	 *            a volume array
+	 */
+	public void play(NBSTick tick, float[] volumes);
 
 	/**
 	 * Closes this IBlockPlayer.
