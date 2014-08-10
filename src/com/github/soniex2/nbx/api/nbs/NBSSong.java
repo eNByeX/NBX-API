4c89b7912fb9a730b50f7a6ac5c89f072d3efd87
SoniEx2 <endermoneymod@gmail.com>
2014-08-10 12:05:08 -0300

    Add NBSSong.read/write

--- src/com/github/soniex2/nbx/api/nbs/NBSSong.java
+++ src/com/github/soniex2/nbx/api/nbs/NBSSong.java
@@ -1,5 +1,10 @@
 package com.github.soniex2.nbx.api.nbs;
 
+import com.github.soniex2.nbx.api.stream.nbs.INBSReader;
+import com.github.soniex2.nbx.api.stream.nbs.INBSWriter;
+
+import java.io.IOException;
+
 /**
  * @author soniex2
  */
@@ -10,6 +15,12 @@ public class NBSSong {
     public NBSSong(NBSSongData song, NBSHeader header) {
         this.songData = song;
         this.header = header;
+        header.setLayers(songData.getLayers());
+        header.setTicks(songData.getTicks());
+    }
+
+    public NBSSong(NBSHeader header, NBSSongData song) {
+        this(song, header);
     }
 
     /**
@@ -25,6 +36,19 @@ public class NBSSong {
 
     public NBSHeader getHeader() {
         return header;
+    }
+
+    public static NBSSong read(INBSReader reader) throws IOException {
+        NBSHeader header = NBSHeader.read(reader);
+        NBSSongData songData = NBSSongData.read(reader, header);
+        return new NBSSong(header, songData);
+    }
+
+    public void write(INBSWriter writer) throws IOException {
+        header.setLayers(songData.getLayers());
+        header.setTicks(songData.getTicks());
+        header.write(writer);
+        songData.write(writer);
     }
 
 }
