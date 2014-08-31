1aefa566206f7c063fbebdb916ffe6b26ba8052d
SoniEx2 <endermoneymod@gmail.com>
2014-08-31 15:46:27 -0300

    Add WIP sNTH chunk

---
+++ src/main/java/com/github/soniex2/nbx/api/nbx/extension/NBXTimeHint.java
@@ -0,0 +1,31 @@
+package com.github.soniex2.nbx.api.nbx.extension;
+
+import com.github.soniex2.nbx.api.nbx.chunk.IChunkable;
+import com.github.soniex2.nbx.api.nbx.chunk.INBXChunk;
+
+/**
+ * @author soniex2
+ */
+// TODO
+public class NBXTimeHint implements IChunkable, INBXChunk {
+    @Override
+    public void fromChunk(INBXChunk chunk) {
+
+    }
+
+    @Override
+    public INBXChunk toChunk() {
+        return null;
+    }
+
+    @Override
+    public String getChunkId() {
+        // [s]ong [N]ote [T]ime [H]int
+        return "sNTH";
+    }
+
+    @Override
+    public byte[] getChunkData() {
+        return new byte[0];
+    }
+}
